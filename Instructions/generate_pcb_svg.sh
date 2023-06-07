ab3453ac6ab799ae97ee8e99855bc0f44bf557c2
Electro707 <general@electro707.com>
2023-06-06 22:53:14 -0400

    Updated instructions. Up-rev in KiCAD files

--- Instructions/generate_pcb_svg.sh
+++ Instructions/generate_pcb_svg.sh
@@ -1,4 +1,4 @@
+#!/bin/bash
 # Bash file to generate the PCBs as a drawing using pcbdraw
-
-pcbdraw plot -l /home/electro/Documents/git/PcbDraw-Lib/KiCAD-base -m "../KiCAD Files/LED PCB/pcbdraw_remap.json" -s set-black-hasl "../KiCAD Files/LED PCB/PCB.kicad_pcb" gen_pcb_led.svg
-pcbdraw plot -l /home/electro/Documents/git/PcbDraw-Lib/KiCAD-base -m "../KiCAD Files/Driver PCB/pcbdraw_remap.json"  --resistor-flip "R15,R14,R13,R12,R11,R10" -s set-black-hasl "../KiCAD Files/Driver PCB/PCB.kicad_pcb" gen_pcb_driver.svg
+pcbdraw plot -l /mnt/aux1/git/PcbDraw-Lib/KiCAD-base -m "../KiCAD Files/LED PCB/pcbdraw_remap.json" -s set-black-hasl "../KiCAD Files/LED PCB/PCB.kicad_pcb" gen_pcb_led.svg
+pcbdraw plot -l /mnt/aux1/git/PcbDraw-Lib/KiCAD-base -m "../KiCAD Files/Driver PCB/pcbdraw_remap.json"  --resistor-flip "R15,R14,R13,R12,R11,R10" -s set-black-hasl "../KiCAD Files/Driver PCB/PCB.kicad_pcb" gen_pcb_driver.svg
--- Instructions/instrunctions_svg_to_png.sh
+++ Instructions/instrunctions_svg_to_png.sh
@@ -1,6 +1,8 @@
+#!/bin/bash
 mkdir -p "$PWD"/png
-for file in $PWD/*.svg
+for file in "$PWD"/*.svg
 do
   filename=$(basename "$file")
   inkscape -d 300 -C -o "$PWD/png/${filename%.svg}.png" "$file" 
+  echo $file
 done
